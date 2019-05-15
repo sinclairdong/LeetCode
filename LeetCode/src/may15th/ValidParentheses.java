e3381e72092d3e2ce2af6a8169d1747fd997071e
Yuesen <sinclairdong@gmail.com>
2019-05-15 18:13:43 -0400

    add new problem

---
+++ LeetCode/src/may15th/ValidParentheses.java
@@ -0,0 +1,32 @@
+package may15th;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+  public static void main(String[] args) {
+    System.out.println(new ValidParentheses().isValid("()[]{}"));
+  }
+
+  public boolean isValid(String s) {
+
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (isLeft(c)) {
+        stack.push(c);
+      } else {
+        char last = stack.pop();
+        if (last - c > 5) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public boolean isLeft(char c) {
+    return c == '(' || c == '[' || c == '{';
+  }
+}
