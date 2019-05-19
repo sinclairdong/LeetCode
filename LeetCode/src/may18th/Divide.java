dc0093478aec5a946dd30575b11f713f28759423
Yuesen Dong <sinclairdong@gamil.com>
2019-05-19 09:15:01 -0400

    add void main

--- LeetCode/src/may18th/Divide.java
+++ LeetCode/src/may18th/Divide.java
@@ -1,11 +1,17 @@
 package may18th;
 
 public class Divide {
+
+  public static void main(String[] args) {
+    System.out.println(new Divide().divide(6, 3));
+  }
+
   public int divide(int dividend, int divisor) {
     boolean sign_a = divisor < 0;
     boolean sign_b = dividend < 0;
-    divisor = Math.abs(divisor);
     dividend = Math.abs(dividend);
+    divisor = Math.abs(divisor);
+
     int result = 0;
     while (dividend >= divisor) {
       result++;
